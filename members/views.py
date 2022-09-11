cd5f473c2d5173ce82e32c116ca18c11cd0e1f11
Vinícius Bitencourt Pereira <viniciusbitencourtpereira@gmail.com>
2022-09-11 08:47:20 -0300

    Seed no banco e retorno dos dados no browser

--- members/views.py
+++ members/views.py
@@ -1,8 +1,15 @@
+from pickletools import read_uint1
+
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
 
+from .models import Members
+
 
 def index(request):
-    template = loader.get_template('index.html')
-    return HttpResponse(template.render())
+    my_members = Members.objects.all().values()
+    output = ""
+    for x in my_members:
+        output += x['firstname']
+    return HttpResponse(output)
